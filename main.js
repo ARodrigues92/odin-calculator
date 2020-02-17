67e7062154dbe7d9d7b0baf0e56127f15d212d6a
André Rodrigues <afbr17@gmail.com>
2020-02-17 18:20:29 +0000

    Add EventListeners to buttons and the function to print their values in the display

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
+const buttons = document.querySelectorAll("button");
+const pValue = document.getElementById("value");
+let value = "";
 
+function display (){
+  pValue.innerHTML = value;
+}
 
-
-
+buttons.forEach ((button) => {
+  button.addEventListener("click",() => {
+    value += button.value;
+    display();
+  });
+});
