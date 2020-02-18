c4dc803085879a02a135b30b16fc90478ae6368f
André Rodrigues <afbr17@gmail.com>
2020-02-18 21:37:26 +0000

    Add code for calculations and display of operations and results

--- main.js
+++ main.js
@@ -4,18 +4,51 @@ const del = document.getElementById("delete");
 const dot = document.getElementById("dot");
 const equals = document.getElementById("equals");
 const clear = document.getElementById("clear");
-const pValue = document.getElementById("value");
+const displayValue = document.getElementById("value");
+const displayOperation = document.getElementById("operation");
 let currentValue = "";
+let operation = "";
+let operationValues = [];
 let dotStatus = 0;
 let decimals = 0;
 
 function display (){
-  pValue.innerHTML = currentValue;
+  displayValue.innerHTML = currentValue;
+  displayOperation.innerHTML = operation;
+}
+
+function clearValue(){
+  displayValue.innerHTML = "";
+}
+
+function operate (){
+  for (let i=1;i<operationValues.length-1;i+=2){
+    if (operationValues[i] === "*"){
+      operationValues[i-1] = operationValues[i-1] * operationValues[i+1];
+      operationValues.splice(i,2);
+      i -= 2;
+    }else if (operationValues[i] === "/"){
+      operationValues[i-1] = operationValues[i-1] / operationValues[i+1];
+      operationValues.splice(i,2);
+      i -= 2;
+    }
+  }
+  for (i=0;i<operationValues.length;i++){
+    if (operationValues[i] === "+"){
+      operationValues[i-1] = operationValues[i-1] + operationValues[i+1];
+      operationValues.splice(i,2);
+      i -= 2;
+    }else if (operationValues[i] === "-"){
+      operationValues[i-1] = operationValues[i-1] - operationValues[i+1];
+      operationValues.splice(i,2);
+      i -= 2;
+    }
+  }
+  currentValue = operationValues[0];
 }
 
 numberButtons.forEach ((button) => {
   button.addEventListener("click",() => {
-    console.log(button.value);
     if (dotStatus === 0 && decimals === 0){
       currentValue += button.value;
     }else if(dotStatus === 1 && decimals === 0){
@@ -28,29 +61,37 @@ numberButtons.forEach ((button) => {
 
 operatorButtons.forEach ((button) => {
   button.addEventListener("click",() => {
-    console.log(button.value);
+    operation += currentValue + button.value;
+    operationValues.push (parseFloat(currentValue));
+    operationValues.push (button.value);
+    currentValue = "";
+    display();
+    clearValue();
   });
 });
 
 del.addEventListener ("click", () => {
-  console.log(del.value);
   currentValue = currentValue.slice(0, -1);
   display();
-})
+});
 
 dot.addEventListener ("click", () => {
-  console.log(dot.value);
   if (dotStatus === 0){
     currentValue += dot.value;
     dotStatus = 1;
   }
-})
+});
 
 clear.addEventListener ("click", () => {
-  console.log(clear.value);
-})
+  //console.log(clear.value);
+});
 
 equals.addEventListener ("click", () => {
-  console.log(equals.value);
-})
+  operationValues.push (parseFloat(currentValue));
+  clearValue();
+  operate();
+  operation = "";
+  operationValues.pop();
+  display();
+});
 
