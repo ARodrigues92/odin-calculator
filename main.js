a0d1bfa4b62526bfeb4519175e5d09ae3fe4f353
André Rodrigues <afbr17@gmail.com>
2020-02-18 18:30:39 +0000

    Change how EventListeners are added

--- main.js
+++ main.js
@@ -1,14 +1,43 @@
-const buttons = document.querySelectorAll("button");
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const del = document.getElementById("delete");
+const dot = document.getElementById("dot");
+const equals = document.getElementById("equals");
+const clear = document.getElementById("clear");
 const pValue = document.getElementById("value");
-let value = "";
+let currentValue = "";
 
 function display (){
-  pValue.innerHTML = value;
+  pValue.innerHTML = currentValue;
 }
 
-buttons.forEach ((button) => {
+numberButtons.forEach ((button) => {
   button.addEventListener("click",() => {
-    value += button.value;
+    console.log(button.value);
+    currentValue += button.value;
     display();
   });
 });
+
+operatorButtons.forEach ((button) => {
+  button.addEventListener("click",() => {
+    console.log(button.value);
+  });
+});
+
+del.addEventListener ("click", () => {
+  console.log(del.value);
+})
+
+dot.addEventListener ("click", () => {
+  console.log(dot.value);
+})
+
+clear.addEventListener ("click", () => {
+  console.log(clear.value);
+})
+
+equals.addEventListener ("click", () => {
+  console.log(equals.value);
+})
+
